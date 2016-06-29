09e51f5b40bc286d158fc823adb30e3ac92de395
gyeon <gyeon19@gmail.com>
2016-06-29 18:47:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'sinatra/base'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,7 +8,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts/new' do 
+    erb :new
+  end
+
+  post '/posts/new' do
+    @blog_post = Post.create(params)
+    redirect "/posts"
+  end
+
+  get '/posts' do
+    @all_posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do 
+    @post = Post.find(params[:id]) ##find the id first
+    @post.name = params["name"]
+    @post.content = params["content"]
+    @post.save ##syntax is passing through what we're updating
+    redirect "/posts/#{@post.id}" ##interpolate because url is a string, and will look for a :id and not a number id, need the instance of post
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id]).destroy
+    "#{@post.name} was deleted"
+    # redirect "/posts"
+  end
+
 end
